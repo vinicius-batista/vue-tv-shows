347044925dd33aadee14e4db3797af28989ec281
Vinicius Batista <viniciusbfs2012@gmail.com>
2018-03-25 16:08:34 -0300

    Finish TvShowDetails

--- src/views/TvShowDetails.js
+++ src/views/TvShowDetails.js
@@ -1,4 +1,5 @@
 import { findShowById } from '@/services/shows'
+import { map, split, join } from 'ramda'
 
 export default {
   name: 'TvShowDetails',
@@ -16,9 +17,49 @@ export default {
       return this.$route.params.id
     }
   },
+  methods: {
+    formatDate (date) {
+      const [year, month, day] = split('-', date)
+      return join('/', [month, day, year])
+    }
+  },
   render () {
     return (
-      <div></div>
+      <v-layout wrap row class="show-details">
+        <v-flex
+          sm5
+          offset-sm1
+          md2
+          offset-md2
+        >
+          <img src={this.show.image.original}/>
+        </v-flex>
+        <v-flex
+          sm5
+          md6
+        >
+          <div class="headline">{this.show.name}</div>
+          <div class="body-1" domPropsInnerHTML={this.show.summary}></div>
+          <div class="body-2">
+            <span>Genres:</span>
+            {map((genre) => (
+              <v-chip>{genre}</v-chip>
+            ), this.show.genres)}
+          </div>
+          <div>
+            <span class="body-2">Release date:</span>
+            <span class="body-1">{this.formatDate(this.show.premiered)}</span>
+          </div>
+          <div>
+            <span class="body-2">Rating:</span>
+            <span class="body-1">{this.show.rating.average}</span>
+          </div>
+          <div>
+            <span class="body-2">Website:</span>
+            <a class="body-1" href={this.show.officialSite}>{this.show.officialSite}</a>
+          </div>
+        </v-flex>
+      </v-layout>
     )
   }
 }
